9011521e3a173acd0036dcfae1cf2ae6c0193bc9
Piotr Sarna <95252277+sarnapiotr@users.noreply.github.com>
2025-05-31 14:58:38 +0200

    LF Graph Coloring

--- KolorowanieGrafow.cpp
+++ KolorowanieGrafow.cpp
@@ -14,31 +14,31 @@ void chooseGraphMatrix(int**& graphMatrix, int& vertexCount) {
         vertexCount = 3;
         graphMatrix = new int* [vertexCount] {
             new int[vertexCount] {0, 1, 1},
-                new int[vertexCount] {1, 0, 1},
-                new int[vertexCount] {1, 1, 0}};
+            new int[vertexCount] {1, 0, 1},
+            new int[vertexCount] {1, 1, 0}};
         break;
     case 2:
         vertexCount = 5;
         graphMatrix = new int* [vertexCount] {
-            new int[vertexCount] {0, 1, 1, 0, 0},
-                new int[vertexCount] {1, 0, 1, 1, 0},
-                new int[vertexCount] {1, 1, 0, 1, 1},
-                new int[vertexCount] {0, 1, 1, 0, 1},
-                new int[vertexCount] {0, 0, 1, 1, 0}};
+            new int[vertexCount] {0, 1, 1, 1, 1},
+            new int[vertexCount] {1, 0, 1, 1, 1},
+            new int[vertexCount] {1, 1, 0, 1, 0},
+            new int[vertexCount] {1, 1, 1, 0, 1},
+            new int[vertexCount] {1, 1, 0, 1, 0}};
         break;
     case 3:
         vertexCount = 10;
         graphMatrix = new int* [vertexCount] {
-            new int[vertexCount] {0, 1, 0, 0, 1, 0, 0, 0, 1, 0},
-                new int[vertexCount] {1, 0, 1, 0, 0, 0, 1, 0, 0, 0},
-                new int[vertexCount] {0, 1, 0, 1, 0, 0, 0, 0, 0, 1},
-                new int[vertexCount] {0, 0, 1, 0, 1, 1, 0, 0, 0, 0},
-                new int[vertexCount] {1, 0, 0, 1, 0, 0, 0, 1, 0, 0},
-                new int[vertexCount] {0, 0, 0, 1, 0, 0, 1, 0, 1, 0},
-                new int[vertexCount] {0, 1, 0, 0, 0, 1, 0, 1, 0, 0},
-                new int[vertexCount] {0, 0, 0, 0, 1, 0, 1, 0, 1, 1},
-                new int[vertexCount] {1, 0, 0, 0, 0, 1, 0, 1, 0, 0},
-                new int[vertexCount] {0, 0, 1, 0, 0, 0, 0, 1, 0, 0}};
+            new int[vertexCount] {0, 1, 1, 1, 1, 0, 0, 0, 0, 0},
+            new int[vertexCount] {1, 0, 1, 1, 1, 0, 0, 0, 0, 0},
+            new int[vertexCount] {1, 1, 0, 1, 1, 0, 0, 0, 0, 0},
+            new int[vertexCount] {1, 1, 1, 0, 1, 1, 0, 0, 0, 0},
+            new int[vertexCount] {1, 1, 1, 1, 0, 0, 1, 0, 0, 0},
+            new int[vertexCount] {0, 0, 0, 1, 0, 0, 1, 1, 0, 0},
+            new int[vertexCount] {0, 0, 0, 0, 1, 1, 0, 1, 1, 0},
+            new int[vertexCount] {0, 0, 0, 0, 0, 1, 1, 0, 1, 1},
+            new int[vertexCount] {0, 0, 0, 0, 0, 0, 1, 1, 0, 1},
+            new int[vertexCount] {0, 0, 0, 0, 0, 0, 0, 1, 1, 0}};
         break;
     default:
         std::cout << "Niepoprawny wybor\n";
@@ -55,7 +55,7 @@ void printGraph(int**& graphMatrix, const int& vertexCount) {
     }
 }
 
-bool isSafe(int** graphMatrix, int vertexCount, const std::vector<int>& colors, int vertex, int color) {
+bool isSafe(int** graphMatrix, const int& vertexCount, const std::vector<int>& colors, const int& vertex, const int& color) {
     for (int i = 0; i < vertexCount; i++) {
         if (graphMatrix[vertex][i] == 1 && colors[i] == color) {
             return false;
@@ -64,7 +64,7 @@ bool isSafe(int** graphMatrix, int vertexCount, const std::vector<int>& colors, 
     return true;
 }
 
-bool solveColoring(int** graphMatrix, int vertexCount, std::vector<int>& colors, int vertex, int maxColors) {
+bool solveColoring(int** graphMatrix, const int& vertexCount, std::vector<int>& colors, const int& vertex, const int& maxColors) {
     if (vertex == vertexCount) {
         return true;
     }
@@ -81,7 +81,7 @@ bool solveColoring(int** graphMatrix, int vertexCount, std::vector<int>& colors,
     return false;
 }
 
-void exactGraphColoring(int** graphMatrix, int vertexCount) {
+void exactGraphColoring(int** graphMatrix, const int& vertexCount) {
     std::vector<int> colors(vertexCount, 0);
 
     for (int maxColors = 1; maxColors <= vertexCount; maxColors++) {
@@ -95,16 +95,69 @@ void exactGraphColoring(int** graphMatrix, int vertexCount) {
     }
 }
 
-void LFgraphColoring(int**& graphMatrix, const int& vertexCount) {
+struct Vertex {
+    int index, neighboursCount;
+};
 
+std::vector<Vertex> getSortedVertexVector(int** graphMatrix, const int& vertexCount) {
+    std::vector<Vertex> vertexVector(vertexCount);
+
+    for (int i = 0; i < vertexCount; i++) {
+        int neighboursCount = 0;
+        for (int j = 0; j < vertexCount; j++) {
+            if (graphMatrix[i][j] == 1) {
+                neighboursCount++;
+            }
+        }
+        vertexVector[i] = {i, neighboursCount};
+
+    }
+
+    for (int i = 0; i < vertexCount; i++) {
+        for (int j = 0; j < vertexCount; j++) {
+            if (vertexVector[j].neighboursCount > vertexVector[i].neighboursCount) {
+                std::swap(vertexVector[i], vertexVector[j]);
+            }
+        }
+    }
+
+    return vertexVector;
+}
+
+void LFgraphColoring(int**& graphMatrix, const int& vertexCount) {
+    std::vector<int> colors(vertexCount, 0);
+    std::vector<Vertex> vertexVector = getSortedVertexVector(graphMatrix, vertexCount);
+
+    int maxColors = 1;
+    for (int i = 0; i < vertexCount; i++) {
+        int vertexIndex = vertexVector[i].index;
+        bool colored = false;
+
+        for (int j = 1; j <= maxColors; j++) {
+            if (isSafe(graphMatrix, vertexCount, colors, vertexIndex, j)) {
+                colors[vertexIndex] = j;
+                colored = true;
+                break;
+            }
+        }
+
+        if (!colored) {
+            colors[vertexIndex] = ++maxColors;
+        }
+    }
+
+    std::cout << "Minimalna liczba kolorow: " << maxColors << "\n";
+    for (int i = 0; i < vertexCount; i++) {
+        std::cout << "Wierzcholek " << i << ": Kolor " << colors[i] << "\n";
+    }
 }
 
 int main()
 {
     std::cout << "Problem kolorowania grafow\n";
 
-    int** graphMatrix{nullptr};
-    int vertexCount{0};
+    int** graphMatrix{ nullptr };
+    int vertexCount{ 0 };
 
     chooseGraphMatrix(graphMatrix, vertexCount);
     printGraph(graphMatrix, vertexCount);
@@ -116,11 +169,10 @@ int main()
     std::cin >> choice;
 
     if (choice == 1) {
-        std::vector<int> colors(vertexCount, 0);
         exactGraphColoring(graphMatrix, vertexCount);
     }
     else if (choice == 2) {
-        //LFgraphColoring();
+        LFgraphColoring(graphMatrix, vertexCount);
     }
     else {
         std::cout << "Niepoprawny wybor\n";
